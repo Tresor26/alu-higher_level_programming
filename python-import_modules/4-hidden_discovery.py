2a0ba36316b8246bb83635cfcace1fcb1702aa36
Tresor26 <t.nkurunziz@alustudent.com>
2024-10-23 01:38:30 -0700

    print hidden contents

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__":
-    # Get all names defined in the hidden_4 module
+if __name__ == "__main_":
     names = dir(hidden_4)
-    
-    # Filter names that do not start with '__'
+
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
