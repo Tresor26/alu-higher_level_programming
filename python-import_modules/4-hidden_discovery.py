516ea5d3d62187470977f7b38d6a7d526d74abfc
Tresor26 <t.nkurunziz@alustudent.com>
2024-10-23 01:42:08 -0700

    print hidden contents using python3.8

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == "__main__":
+    import hidden_4
 
-if __name__ == "__main_":
-    names = dir(hidden_4)
-
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
     for name in sorted(names):
-        if not name.startswith("__"):
-            print(name)
+        print(name)
