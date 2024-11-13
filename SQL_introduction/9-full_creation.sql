f80fb0de2c0d563dc324a28ea470d93e118291cd
Tresor26 <t.nkurunziz@alustudent.com>
2024-11-13 03:30:21 -0800

    sql command to create table and insert data

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- create new table and insert data
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table(id, name, score) VALUES (1, John, 10),(2, Alexi, 3), (3, Bob, 14), (4, George, 8);
